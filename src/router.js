d437393a2fe56be8636895acd640c1998636e4ec
cgomesj <32676188+cgomesj@users.noreply.github.com>
2019-07-27 15:33:51 -0300

    redirecting route defined

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    { path: "/redirect-me", redirect: { name: "home" } },
     {
       path: "/",
       name: "home",
